24403e8dc9603d7f350388a7e40dd7021dfdd33c
squirrel <107772629+philipkim00@users.noreply.github.com>
2022-09-02 22:30:29 +0900

    어소트락 수업내용과 숙제 파일

---
+++ 어소트락 수업/homework/01.Debug.cpp
@@ -0,0 +1,19 @@
+﻿
+#include <iostream>
+
+int main()
+
+{
+
+    std::cout << "Hello World!\n";
+
+    std::cout << "첫번째 수업!\n";
+
+    std::cout << "멍때리기!\n";
+
+    std::cout << "크흠크흠!\n";
+
+    std::cout << "멍떄리기!\n";
+
+    return 0;
+}
