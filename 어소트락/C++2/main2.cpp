f604e750afeb2f4cfb7d9b459c4110b3cec0f406
squirrel <107772629+philipkim00@users.noreply.github.com>
2022-09-05 17:38:07 +0900

    재귀함수(1)

--- 어소트락/C++2/main2.cpp
+++ 어소트락/C++2/main2.cpp
@@ -2,7 +2,6 @@
 
 // 함수
 // Factorial
-// 8!
 int Factorial(int _iNum)
 {
 	int iValue = 1;
@@ -13,16 +12,18 @@ int Factorial(int _iNum)
 		iValue *= (j + 2);
 	}
 
+	Factorial(10);
+
 	return iValue;
 }
 
+// 재귀함수
+// 가독성, 구현의 용이.
 
 int main()
 {
 	int iValue = Factorial(4);
 	iValue = Factorial(10);
-
-
 
 	return 0;
 }
