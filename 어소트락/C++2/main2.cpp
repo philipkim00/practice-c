da0758755ffc9e010e67fd6ab2dd442d8090a91a
squirrel <107772629+philipkim00@users.noreply.github.com>
2022-09-05 16:51:02 +0900

    함수(3)

--- 어소트락/C++2/main2.cpp
+++ 어소트락/C++2/main2.cpp
@@ -1,87 +1,26 @@
 #include <stdio.h>
 
-int Add(int a, int b)
+// 함수
+// Factorial
+// 8!
+int Factorial(int _iNum)
 {
-	return a + b;
+	int iValue = 1;
+
+	for (int j = 0; j < _iNum - 1; ++j)
+	{
+
+		iValue *= (j + 2);
+	}
+
+	return iValue;
 }
 
-// 함수
+
 int main()
 {
-	int iData = Add(100, 200);
-
-	iData = Add(300, 400);
-
-	iData = Add(222, 700);
-
-	iData = Add(333, 555);
-
-
-
-
-	// 반복문 
-	//for (/*반복자 초기화*/;/*반복자 조건 체크*/;/*반복자 변경*/)
-	//{
-		
-	//} 
-
-	// 0, 1, 2, 3
-	for (int i = 0; i < 4; ++i)
-	{
-		if (i % 2 == 1)
-		{
-			continue;
-		}
-
-		printf("output Test\n");
-	}
-
-	/*int i = 0;
-	while (i < 4)
-	{
-		printf("Output Test\n");
-
-		++i;
-
-		continue;
-
-		break;
-	} */
-
-	int i = 0;
-	int i2 = 1;
-
-	// 단축키
-	// 지정한 구문 주석       : Ctrl + k, c
-	// 지정한 구문 주석 해제  : Ctrl + k, u
-	// Alt Drag
-
-	// 디버깅
-	// 디비겅 시작                     : F5
-	// 다음 중단점까지 코드 실행
-
-	// 중단점 생성 및 해제             : F9
-	// 디버깅 중, 구문 수행            : F10
-	// 디버깅 중, 구문 수행(함수 진입) : f11
-	// 디버깅 중, 디버깅 종료하기 : Shift + F5
-
-	// 콘솔
-	// printf
-	printf("abcdef %d \n", 10);
-	printf("abcdef %f \n", 3.14f);
-
-	for (int i = 0; i < 10; ++i)
-	{
-		printf("Output i : %d \n", i);
-	}
-
-	// scanf
-	int iInput = 0;
-	scanf_s("%d", &iInput);
-
-
-	// 함수가 사용하는 메모리 영역
-	// 스택 메모리 영역
+	int iValue = Factorial(4);
+	iValue = Factorial(10);
 
 
 
